33d0ee7cf703f3344a7783fe7c86c5db78f8c0ec
Edgars Jupatovs <edgaars@gmail.com>
2021-12-01 20:39:49 +0200

    completed day 1

--- day_01/main.py
+++ day_01/main.py
@@ -1,7 +1,7 @@
 with open('nums.txt') as f:
     data = f.read()
     
-nums = data.split()
+nums = [int(num) for num in data.split()]
 
 x = 1
 times_larger = 0
@@ -10,4 +10,20 @@ for i in range(len(nums)-1):
         times_larger+=1
     x+=1
 
-print(f'Larger than previous: {times_larger}')
+print(f'Measurement larger than previous: {times_larger}')
+
+finished = False
+counter = 0
+sum_larger = 0
+while not finished:
+    if counter + 3 < len(nums):
+        first_sum = nums[counter] + nums[counter+1] + nums[counter+2]
+        second_sum = nums[counter + 1] + nums[counter+ 2] + nums[counter+3]
+        if second_sum > first_sum:
+            sum_larger+=1
+        counter+=1
+    else:
+        finished = True
+
+print(f'Sum of measurements in sliding window increases {sum_larger} times.')
+
